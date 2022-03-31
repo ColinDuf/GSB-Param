09763c02fa422ec2a63b72a477d410f5df9e2526
ColinDuf <colin.dufeutrelle@gmail.com>
2022-03-31 23:50:04 +0200

    CSS formulaire inscription

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -6,16 +6,16 @@
             <h1 class="form_title">Connexion</h1>
             <div class="form__message form__message--error"></div>
             <div class="form__input-group">
-                <input type="email" class="form__input" autofocus placeholder="Email" required>
+                <input type="email" class="form__input" name="mail" autofocus placeholder="Email" required>
                 <div class="form__input-error-message"></div>
             </div>
             <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Mot de passe" required>
+                <input type="password" class="form__input" name="mdp" autofocus placeholder="Mot de passe" required>
                 <div class="form__input-error-message"></div>
             </div>
             <button class="form__button" type="submit">Valider</button>
             <p class="form__text">
-                <a class="form__link" href="index.php?uc=gestionConnexion&action=inscription" id="linkCreateAccount">Pas de compte ? Créer un compte</a>
+                <a class="form__link" href="index.php?uc=gestionConnexion&action=inscription" id="linkCreateAccount">Pas de compte ? Créer un compte !</a>
             </p>
         </form>
     </div>
--- vues/v_inscription.php
+++ vues/v_inscription.php
@@ -4,19 +4,42 @@
 <div class="container">
         <form action="index.php?uc=gestionConnexion&action=verif" class="form" id="login" method="POST">
             <h1 class="form_title">Inscription</h1>
-            <div class="form__message form__message--error"></div>
             <div class="form__input-group">
-                <input type="email" class="form__input" autofocus placeholder="Email" required>
-                <div class="form__input-error-message"></div>
+                <input type="text" class="form__input" placeholder="Nom" name="nom" autofocus required>
             </div>
+
             <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Mot de passe" required>
-                <div class="form__input-error-message"></div>
+                <input type="text" class="form__input" placeholder="Prenom" name="prenom" required>
             </div>
+            
+            <div class="form__input-group">
+                <input type="email" class="form__input" placeholder="Email" name="email" required>
+            </div>
+
+            <div class="form__input-group">
+                <input type="text" class="form__input" placeholder="Ville" name="ville" required>
+            </div>
+            
+            <div class="form__input-group">
+                <input type="text" class="form__input" placeholder="Code Postal" name="cp" required>
+            </div>
+            
+            <div class="form__input-group">
+                <input type="text" class="form__input" placeholder="Rue" name="rue" required>
+            </div>
+            
+            <div class="form__input-group">
+                <input type="password" class="form__input" placeholder="Mot de passe" name="pass" required>
+            </div>
+            
+            <div class="form__input-group">
+                <input type="password" class="form__input" placeholder="Mot de passe" name="pass2" required>
+            </div>
+
             <button class="form__button" type="submit">Valider</button>
 
             <p class="form__text">
-                <a class="form__link" href="index.php?uc=gestionConnexion&action=inscription" id="linkCreateAccount">Pas de compte ? Créer un compte</a>
+                <a class="form__link" href="index.php?uc=gestionConnexion&action=seConnecter" >Déja inscrit ! Connecté vous ici !</a>
             </p>
         </form>
     </div>
