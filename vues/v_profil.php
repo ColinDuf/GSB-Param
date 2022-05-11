617272861e9a9c0a2b24ffb688140e1471cf0d75
ColinDuf <colin.dufeutrelle@gmail.com>
2022-05-11 08:22:36 +0200

    Fin profil

--- vues/v_profil.php
+++ vues/v_profil.php
@@ -1,4 +1,17 @@
-<div class=""> Détail du profil :</div>
-
-<div class="">Nom de l'utilisateur : <?php echo $info['nom'] ?></div>
-
+<div class="w-100 d-flex justify-content-center">
+    <section class="d-flex flex-column align-items-center w-25 m-2 border border-secondary rounded">
+        <div class=" m-2"> Détail du profil :</div>
+        <?php
+        echo '<div class="profil py-1"><u>Nom</u> : ' . $info['nom'] . '</div>';
+        echo '<div class="profil py-1"><u>Prenom</u> : ' . $info['prenom'] . '</div>';
+        echo '<div class="profil py-1"><u>Mail</u> : ' . $info['mail'] . '</div>';
+        echo '<div class="profil py-1"><u>Téléphone</u> : ' . $info['tel'] . '</div>';
+        echo '<div class="profil py-1"><u>Ville</u> : ' . $info['ville'] . '</div>';
+        echo '<div class="profil py-1"><u>Adresse</u> : ' . $info['adresse'] . '</div>';
+        echo '<div class="profil py-1"><u>Code Postal</u> : ' . $info['cp'] . '</div>';
+        ?>
+         <form action="index.php?uc=gestionConnexion&action=seDeconnecter" class="form" id="deconnexion" method="POST">
+        <button name="submit" class="btn btn-outline-danger justify-content-center" type="submit">Déconnexion</button>
+         </form>
+</div>
+</section>
